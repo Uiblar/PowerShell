089c8669bbe672b9f3dcdb44e6c989b0682a71fd
schemrah <74898405+schemrah@users.noreply.github.com>
2022-11-15 20:54:12 +0100

    Update ADAccountCheck.ps1

--- ADAccountCheck.ps1
+++ ADAccountCheck.ps1
@@ -1,14 +1,4 @@
-﻿#Array of all DCs, FQDN will have better performance
-$DCs = @(
-	"DC-00004",
-	"DC-00005",
-	"DC-00006",
-	"DC-00007",
-	"DC-00008",
-	"DC-00009",
-	"DC-00010",
-	"DC-00011"
-)
+﻿Clear-Host
 Function ConvertTime{
     Param([Parameter(Mandatory)]$TimeNT)
     Process{
@@ -20,37 +10,50 @@ Function ConvertTime{
 Function NewLine{
 	Write-Host "`n"
 }
+$DCs = @()
+$DCDiscovery = (Get-ADDomainController -Filter * | select name)
+foreach($dc in $DCDiscovery){
+    $DCs += $dc.name
+}
 while($true){
     $LogonList = @()
     $user = ""
     #Get User
     while($user.length -eq 0){$user = Read-Host "Bitte Kürzel eingeben";NewLine}
-    $ADUser = Get-ADUser -identity $user -Properties *
-    #Check Last PW Change
-    Write-Host "Letzter Passwortwechsel: $(ConvertTime -TimeNT $ADUser.pwdLastSet)"
-    Write-Host "Letzter fehlgeschlagener Login: $(get-date $ADUser.LastBadPasswordAttempt -Format "dd.MM.yyyy HH:mm:ss")"
-    #Check Account Lockout Status
-    if ($ADUser.lockedout) {"Konto gesperrt"}
-    else {"Konto nicht gesperrt"}
-    #Check Account Active
-    if ($ADUser.enabled) {"Konto aktiv"}
-    else {"Konto deaktiviert"}
-    #Check PW Life
-    if ($ADUser.passwordexpired) {"Passwort abgelaufen"}
-    else {"Passwort nicht abgelaufen"}
+    try{$ADUser = Get-ADUser -identity $user -Properties *}
+    catch{Write-Host "User not found" -ForegroundColor Red; continue}
     #Return DN
-    "Account befindet sich in OU: $($ADuser.DistinguishedName)"
+    "Account befindet sich in OU: $(try{$ADuser.DistinguishedName}catch{"NOT FOUND"})"
+    #Check Last PW Change
+    Write-Host "Letzter Passwortwechsel: $(try{ConvertTime -TimeNT $ADUser.pwdLastSet}catch{"NOT FOUND"})"
+    Write-Host "Letzter fehlgeschlagener Login: $(try{get-date $ADUser.LastBadPasswordAttempt -Format "dd.MM.yyyy HH:mm:ss"}catch{"NOT FOUND"})"
+    #Check Account Lockout Status
+    if ($ADUser.lockedout) {write-host "Konto gesperrt" -ForegroundColor Red}
+    else {write-host "Konto nicht gesperrt" -ForegroundColor Green}
+    #Check Account Active
+    if ($ADUser.enabled) {write-host "Konto aktiviert" -ForegroundColor Green}
+    else {write-host "Konto deaktiviert" -ForegroundColor Red}
+    #Check PW Life
+    if ($ADUser.passwordexpired) {write-host "Passwort abgelaufen" -ForegroundColor Red}
+    else {write-host "Passwort nicht abgelaufen" -ForegroundColor Green}
+
     #Check on each DC
     foreach ($DC in $DCs){
-        $tmpLogon = Get-ADUser -Identity $user -Server $DC -Properties *
-        $LogonList += New-Object -Type PSObject -Property (@{
-            "Server" = "$DC - "
-            "Time" = $tmpLogon.LastLogon
-        })
+        try{
+            $tmpLogon = Get-ADUser -Identity $user -Server $DC -Properties *
+            $LogonList += New-Object -Type PSObject -Property (@{
+                "Server" = "$DC - "
+             "Time" = $tmpLogon.LastLogon
+            })
+        }
+        catch{$LogonList += "UNAVAILABLE"}
     }
     $LogonList = $LogonList | sort -Property Time
     foreach($Entry in $LogonList){
-        $Entry.Time = ConvertTime -TimeNT $Entry.Time
+        if($Entry.Time){
+            try{$Entry.Time = ConvertTime -TimeNT $Entry.Time}
+            catch{}
+        }
     }
     Write-Host "Letzte Logins an DCs"
     $LogonList | ft -HideTableHeaders
